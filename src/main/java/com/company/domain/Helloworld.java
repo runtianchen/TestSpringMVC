93c680eccf1639ae9333039ffcdf3654ce0d4b1e
chenrt <chenrt@xiaoshouyi.com>
2019-05-09 10:35:06 +0800

    test 2019/5/9 10:34

--- src/main/java/com/company/domain/Helloworld.java
+++ src/main/java/com/company/domain/Helloworld.java
@@ -2,6 +2,9 @@ package com.company.domain;
 
 public class Helloworld {
 
+    public Helloworld() {
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
